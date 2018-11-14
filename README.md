4417b2f4ebc79b7a4525d53149101a20507238ea
Dejan Toteff <deyan8284@gmail.com>
2018-11-14 11:24:00 +0200

    feat: turn bookmarks html to list of links

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# useful-javascript-libraries
-Collection of Javascript libraries
-
-- Integrate with `lazy.helpers`
-
-- Each request change the file on the disk
-
-- It creates a recond in `JSON` file with additional properties as `weight` and `tag`
-
-- manually run script to build the final `Readme`
+# Useful Javascript libraries
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import {readdirSync, readFileSync} from 'fs'
 import rp from 'request-promise'
+import { startsWith, s, remove } from 'rambdax'
+s()
 
-console.log(rp)
+void async function populate(){
+  const [bookmarksFile] = readdirSync(__dirname)
+    .filter(startsWith('bookmarks'))
+
+  if(!bookmarksFile) return
+  const bookmarksContent = readFileSync(
+    `${__dirname}/${bookmarksFile}`
+  ).toString()
+
+  const y = bookmarksContent
+    .match(/HREF="[a-zA-Z:\/\.\?#]+"/g)
+    .s(x => x.map(remove(/HREF="|"/g)))
+  console.log({y})
+}()
