8da7b1b62a329c9e23ebbe6612d96c07ab205e2d
Dejan Toteff <deyan8284@gmail.com>
2018-11-14 22:52:07 +0200

    feat: compare module

---
+++ src/_modules/compare.js
@@ -0,0 +1,18 @@
+const { dateDiff } = require('../_helpers/dateDiff')
+
+const UPDATED = 6
+const  ISSUES = 9
+const STARS = 1
+
+function compare(a,b){
+  const updated = dateDiff(b.updated_at, a.updated_at)*UPDATED
+  const issues = Math.floor(b.open_issues - a.open_issues)*ISSUES
+  const stars = Math.floor(a.stargazers_count - b.stargazers_count)*STARS
+
+  const total = updated + issues + stars
+  console.log({total, updated, issues, stars})
+
+  return total
+}
+
+exports.compare = compare
--- src/_modules/compare.spec.js
+++ src/_modules/compare.spec.js
@@ -3,8 +3,35 @@
  */
 const {compare} = require('./compare')
 const {repoData} = require('./repoData')
-const {sortBy} = require('rambdax')
+const {sort} = require('rambdax')
 
+const first = {
+  updated_at: '2018-11-12T13:51:57Z',
+  stargazers_count: 37,
+  open_issues: 0
+}
+const second = {
+  updated_at: '2018-11-01T07:41:55Z',
+  stargazers_count: 183,
+  open_issues: 9
+}
+const third = {
+  updated_at: '2018-09-14T07:41:55Z',
+  stargazers_count: 407,
+  open_issues: 30
+}
+
+test('first is better', () => {
+  expect(
+    compare(first, second)
+  ).toBeGreaterThan(0)
+})
+
+test('second is better', () => {
+  expect(
+    compare(second, third)
+  ).toBeGreaterThan(0)
+})
 
 test('', async () => {
   const firstURL = 'https://github.com/selfrefactor/rambda'
@@ -12,8 +39,8 @@ test('', async () => {
 
   const firstData = await repoData(firstURL)
   const secondData = await repoData(secondURL)
-  const sorted = sortBy(compare, [firstData, secondData])
+  const sorted = sort(compare, [firstData, secondData])
   expect(
     sorted[0]
-  ).toEqual(firstData)
+  ).toEqual(secondData)
 })
